67ed89d075696826a4e1834ac4b585996c584963
marcal-morais <marcal.2@gmail.com>
2022-10-24 16:36:19 -0300

    Projeto finalizado

--- src/index.js
+++ src/index.js
@@ -10,19 +10,56 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const user = users.find(u => u.username === username);
+  if (!user)
+    return response.status(404).json({ error: "Usuário inexistente" });
+  request.user = user;
+  next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const user = request.user;
+  if (user.pro || user.todos.length < 10)
+    next();
+  else
+    return response.status(403).json({ error: "Limite do plano gratuito atingido" })
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  const user = users.find(u => u.username === username);
+
+  if (!user)
+    return response.status(404).json({ error: "Usuário inexistente" });
+
+  if (!validate(id))
+    return response.status(400).json({ error: "ID inválido" });
+
+  const todo = user.todos.find(t => t.id === id);
+
+  if (!todo)
+    return response.status(404).json({ error: "Todo inexistente" });
+
+  request.user = user;
+  request.todo = todo;
+
+  next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find(u => u.id === id);
+
+  if (!user)
+    return response.status(404).json({ error: "Usuário inexistente" });
+
+  request.user = user;
+
+  next();
 }
 
 app.post('/users', (request, response) => {
